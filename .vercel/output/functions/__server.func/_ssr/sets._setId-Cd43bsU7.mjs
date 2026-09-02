a346016b8e03153b6e00f06894acfb7c44767933
mahsum <mahsum@Mac.fritz.box>
2026-09-02 11:17:04 +0200

    remove stale prebuilt vercel output

--- .vercel/output/functions/__server.func/_ssr/sets._setId-Cd43bsU7.mjs
+++
@@ -1,9 +0,0 @@
-import { p as Outlet } from "../_libs/@tanstack/react-router+[...].mjs";
-import { P as require_jsx_runtime } from "../_libs/@radix-ui/react-alert-dialog+[...].mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/sets._setId-Cd43bsU7.js
-var import_jsx_runtime = require_jsx_runtime();
-function SetLayout() {
-	return /* @__PURE__ */ (0, import_jsx_runtime.jsx)(Outlet, {});
-}
-//#endregion
-export { SetLayout as component };
